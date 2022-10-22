19f65531e6c8c7e7194683e980b94f3f76250c69
chandan-singh <12348957@gmail.com>
2022-10-22 09:42:13 +0530

    logic added

--- login.js
+++ login.js
@@ -1,4 +1,9 @@
 let rp='1234';
-let re='qwe';
+let re='qwe'
 let ee='skjvkjn';
 let ep='sdjff';
+if (rp==ee && rp==re){
+ console.log('Login sucssesssful');
+}else{
+    console.log('Invalid Email and Pasword');
+}
